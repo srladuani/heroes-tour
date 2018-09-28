fbff28292aaff7b7d04ce6a7368805b6f5398561
Sarwan Kumar <srladuani@gmail.com>
2018-09-28 17:27:52 +0500

    getHero(): url fixed

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -33,7 +33,7 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
- save(): void {
+  save(): void {
     this.heroService.updateHero(this.hero)
       .subscribe(() => this.goBack());
   }
